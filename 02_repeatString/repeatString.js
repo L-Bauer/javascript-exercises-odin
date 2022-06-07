d58a5d2c4891ea12a0831f100333188148c53be5
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-07 14:11:18 -0500

    Setup loop to match tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let string = '';
+    if (num > 0) {
+        for (let i = num; i < num; i++) {
+            string += word;
+        }
+    }
+    else {
+        return 'Error';
+    }
 };
 
 // Do not edit below this line
