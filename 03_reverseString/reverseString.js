004fe3b96f587529a3714f6ab2c90e6116fd3ba6
Lucas Bauer <lucassoccer95@gmail.com>
2022-06-08 21:03:30 -0500

    Currently does not run through loop
    There is no console log output from inside the loop. The next
    thing to try is just creating a basic loop and ignore using
    the array. When a plain loop works, then try adding the array back in.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,13 +2,9 @@ const reverseString = function(string) {
     const stringArray = string.split('');
     const reverseArray = [];
     let num = stringArray.length;
-    console.log(num);
     for (let i = num; i < num; i++) {
         reverseArray.unshift(stringArray[i]);
         console.log(string);
-    }
-    if (reverseArray.length == stringArray.length) {
-        return reverseArray;
     }
 };
 
