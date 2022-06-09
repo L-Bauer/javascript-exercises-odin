38df960dd98c4a4eeeebc12dcf30056a36a44cef
Lucas Bauer <lucassoccer95@gmail.com>
2022-06-08 20:26:05 -0500

    Intitial Commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('')
+    for (let i = stringArray.length(); i > stringArray.length(); i++) {
+        console.log(stringArray[i])
+    }
 };
 
 // Do not edit below this line
