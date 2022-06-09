167bc0141bd81f02214b47e49dd2ce9c995435f0
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-09 08:34:08 -0500

    Fix for loop
    The loops initial value was set to the end condition. The initial value
    needs to be less than the condition check.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,7 +2,7 @@ const reverseString = function(string) {
     const stringArray = string.split('');
     const reverseArray = [];
     let num = stringArray.length;
-    for (let i = num; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         reverseArray.unshift(stringArray[i]);
         console.log(string);
     }
