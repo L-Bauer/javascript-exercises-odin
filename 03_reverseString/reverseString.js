978c7c65eb8439f163b4ec4be22b0d116268dde7
Lucas Bauer <lucassoccer95@gmail.com>
2022-06-08 21:01:26 -0500

    Delete return statement
    Having issues with the return statement

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,15 @@
 const reverseString = function(string) {
     const stringArray = string.split('');
-    const reverseArray = [1];
-    for (let i = stringArray.length; i < stringArray.length ; i++) {
+    const reverseArray = [];
+    let num = stringArray.length;
+    console.log(num);
+    for (let i = num; i < num; i++) {
         reverseArray.unshift(stringArray[i]);
         console.log(string);
     }
-    return reverseArray;
+    if (reverseArray.length == stringArray.length) {
+        return reverseArray;
+    }
 };
 
 // Do not edit below this line
