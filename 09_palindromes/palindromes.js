940a279c8f8db1e85ef602431808ab75867aec14
lucas <lucassoccer95@gmai.com>
2022-07-16 21:12:30 -0500

    Implement loop

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (palArray) {
+    const revPalArray = [];
+    let palLength = palArray.length;
+    for (let i = 0; i <= palLength; i++) {
+        revPalArray.unshift(palArray[i])
+    }
+    return revPalArray;
 };
+console.log(palindromes('racecar'))
 
 // Do not edit below this line
 module.exports = palindromes;
